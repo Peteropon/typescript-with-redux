0c2e134b85bc0ce613c9904f2939f5dbe61796c9
Petros Tsavdaridis <peteropon@gmail.com>
2021-01-22 17:34:32 +0100

    Set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import './App.css';
+import Header from "./components/Header"
+import { Route } from "react-router-dom";
+import HomePage from "./components/HomePage"
+import ResultsPage from "./components/ResultsPage"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Oderland Test
-        </p>
-      </header>
-      <main>
-        <button>Click me</button>
-      </main>
+      <Header/>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route exact path="/results" component={ResultsPage}></Route>
+
+
     </div>
   );
 }
