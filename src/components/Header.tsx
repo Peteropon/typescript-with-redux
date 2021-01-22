edc5250fcbba47186046647c98824dac89ad228b
Petros Tsavdaridis <peteropon@gmail.com>
2021-01-22 17:32:58 +0100

    Created Header comp with nav.

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <nav>
+            <NavLink to="/" exact>Home</NavLink>{" | "}
+            <NavLink to="/results">Results</NavLink>
+            <header className="App-header">
+                <p>
+                Oderland Test
+                </p>
+            </header>
+        </nav>
+    )
+}
+
+export default Header;
