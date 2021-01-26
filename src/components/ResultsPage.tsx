3418fbdd2c406b503eda0aa19ea3e41151fea5dd
Petros Tsavdaridis <peteropon@gmail.com>
2021-01-26 17:08:21 +0100

    Connected page to redux store.

--- src/components/ResultsPage.tsx
+++ src/components/ResultsPage.tsx
@@ -1,9 +1,26 @@
-const ResultsPage = () => {
+import { connect } from "react-redux";
+import { CounterState } from "../redux/types";
+
+interface PropsFromState {
+  counter: number;
+}
+
+type Props = PropsFromState;
+
+const ResultsPage = ({ counter }: Props) => {
   return (
     <section>
-      <p>Here are the results.</p>
+      <p>
+        Here are the results: <span>{counter}</span>
+      </p>
     </section>
   );
 };
 
-export default ResultsPage;
+function mapStateToProps(state: CounterState) {
+  return {
+    counter: state.counter,
+  };
+}
+
+export default connect(mapStateToProps)(ResultsPage);
