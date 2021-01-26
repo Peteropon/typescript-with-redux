3357543b840edb50004f7132b324f32c65227f1e
Petros Tsavdaridis <peteropon@gmail.com>
2021-01-26 17:07:06 +0100

    Applied middleware with devtools.

--- src/redux/configureStore.tsx
+++ src/redux/configureStore.tsx
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import { applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
 
 export default store;
